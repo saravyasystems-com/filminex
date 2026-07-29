e5fade680c66c840b8c1287be1feb286352591b9
saravyasystems-com <saravyasystems@gmail.com>
2026-07-29 05:36:15 +0530

    Enforce modular monolith boundaries

--- apps/filminex-api/build.gradle.kts
+++ apps/filminex-api/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    testImplementation("com.tngtech.archunit:archunit-junit5:1.4.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
