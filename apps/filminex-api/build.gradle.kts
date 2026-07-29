e79c96e723d0c52beafd0da696a8bdc8a888feef
saravyasystems-com <saravyasystems@gmail.com>
2026-07-29 05:49:14 +0530

    Fix Spring dependency resolution

--- apps/filminex-api/build.gradle.kts
+++ apps/filminex-api/build.gradle.kts
@@ -10,6 +10,8 @@ java {
 }
 
 dependencies {
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:4.1.0"))
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
